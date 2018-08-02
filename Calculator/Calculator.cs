371eafb960962a8003296a1b83b36246c4dd3749
Ganesh <gtalk10p@gmail.com>
2018-08-02 12:41:33 +0530

    Initial checkin
    Initial checkin

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculators
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            if (a < 10)
+            {
+                return a + b;
+            }
+            else
+            {
+                return a - 10 + b;
+            }
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            if (a < 10)
+            {
+                return a * b;
+            }
+            else
+            {
+                return (a - 10) * b;
+            }
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ CalculatorTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Calculators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Calculator calculator = new Calculator(); 
+
+        [TestMethod]
+        public void TestMethod1_Add()
+        {
+            Assert.IsTrue(7 == calculator.Add(3, 4));
+        }
+
+        [TestMethod]
+        public void TestMethod2_Multiply()
+        {
+            Assert.IsTrue(12 == calculator.Multiply(3, 4));
+        }
+
+        [TestMethod]
+        public void TestMethod3_Add()
+        {
+            Assert.IsTrue(7 == calculator.Add(13, 14));
+        }
+
+        [TestMethod]
+        public void TestMethod4_Multiply()
+        {
+            Assert.IsTrue(12 == calculator.Multiply(13, 14));
+        }
+    }
+}
