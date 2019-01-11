971c1209fc8888239ebb7a47d7fb9b91390984ac
Ganesh Pande <ganesp@microsoft.com>
2019-01-11 11:05:05 +0530

    Added fron end tests

---
+++ FrontEndTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FrontEnd;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FrontEndTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1_Add()
+        {
+            FrontEnd.FrontEnd frontEnd = new FrontEnd.FrontEnd();
+
+            frontEnd.Add();
+        }
+    }
+}
