99b6611b00a6cac139b065345b9ad7ed1aebe91a
Ganesh Pande <ganesp@microsoft.com>
2019-01-11 10:24:43 +0530

    Added front end

---
+++ FrontEnd/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontEnd
+{
+    public class FrontEnd
+    {
+        public void Add()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(1, 2);
+        }
+    }
+}
